8d97de55f518be0373f127fa5bfdd0f28c624d0f
Tony Lea <tony@tonys-mbp-2.home>
2019-10-24 11:14:55 -0700

    initial commit

---
+++ app/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
+
+class User extends Authenticatable
+{
+    use Notifiable, Billable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto">
+        <div class="flex flex-wrap justify-center">
+            <div class="w-full max-w-sm">
+                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+
+                    <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                        {{ __('Register') }}
+                    </div>
+
+                    <form id="signup-form" class="w-full p-6" method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="name" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('Name') }}:
+                            </label>
+
+                            <input id="name" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('name') ? ' border-red-500' : '' }}" id="name" name="name" value="{{ old('name') }}" required autofocus>
+
+                            @if ($errors->has('name'))
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $errors->first('name') }}
+                                </p>
+                            @endif
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('E-Mail Address') }}:
+                            </label>
+
+                            <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('email') ? ' border-red-500' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                            @if ($errors->has('email'))
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $errors->first('email') }}
+                                </p>
+                            @endif
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('Password') }}:
+                            </label>
+
+                            <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('password') ? ' border-red-500' : '' }}" name="password" required>
+
+                            @if ($errors->has('password'))
+                                <p class="text-red-500 text-xs italic mt-4">
+                                    {{ $errors->first('password') }}
+                                </p>
+                            @endif
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="password-confirm" class="block text-gray-700 text-sm font-bold mb-2">
+                                {{ __('Confirm Password') }}:
+                            </label>
+
+                            <input id="password-confirm" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="password_confirmation" required>
+                        </div>
+
+                        <div class="flex flex-wrap mb-6">
+                            <label for="card-element" class="block text-gray-700 text-sm font-bold mb-2">
+                                Credit Card Info
+                            </label>
+                            <div id="card-element" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></div>
+                            <div id="card-errors" class="text-red-400 text-bold mt-2 text-sm font-medium"></div>
+                        </div>
+
+                        <!-- Stripe Elements Placeholder -->
+                        <div class="flex flex-wrap mt-6">
+                            <button type="submit" id="card-button" class="inline-block align-middle text-center select-none border font-bold whitespace-no-wrap py-2 px-4 rounded text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700">
+                                {{ __('Register') }}
+                            </button>
+
+                            <p class="w-full text-xs text-center text-gray-700 mt-8 -mb-4">
+                                Already have an account?
+                                <a class="text-blue-500 hover:text-blue-700 no-underline" href="{{ route('login') }}">
+                                    Login
+                                </a>
+                            </p>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@endsection
+
+@section('javascript')
+    <script src="https://js.stripe.com/v3/"></script>
+
+    <script>
+        const stripe = Stripe('{{ env("STRIPE_KEY") }}');
+        console.log(stripe);
+
+        const elements = stripe.elements();
+        const cardElement = elements.create('card');
+
+        cardElement.mount('#card-element');
+
+        const cardHolderName = document.getElementById('name');
+        const cardButton = document.getElementById('card-button');
+        const clientSecret = cardButton.dataset.secret;
+        let validCard = false;
+        const cardError = document.getElementById('card-errors');
+
+        cardElement.addEventListener('change', function(event) {
+            
+            if (event.error) {
+                validCard = false;
+                cardError.textContent = event.error.message;
+            } else {
+                validCard = true;
+                cardError.textContent = '';
+            }
+        });
+
+        var form = document.getElementById('signup-form');
+
+        form.addEventListener('submit', async (e) => {
+            event.preventDefault();
+
+            const { paymentMethod, error } = await stripe.createPaymentMethod(
+                'card', cardElement, {
+                    billing_details: { name: cardHolderName.value }
+                }
+            );
+
+            if (error) {
+                // Display "error.message" to the user...
+                console.log(error);
+            } else {
+                // The card has been verified successfully...
+                var hiddenInput = document.createElement('input');
+                hiddenInput.setAttribute('type', 'hidden');
+                hiddenInput.setAttribute('name', 'payment_method');
+                hiddenInput.setAttribute('value', paymentMethod.id);
+                form.appendChild(hiddenInput);
+                // Submit the form
+                form.submit();
+            }
+
+        });
+    
+    </script>
+@endsection
